299a55291ece6005d288ebeb2b7e196bfd332bcd
hooijing <67728587+hooijing@users.noreply.github.com>
2020-07-06 19:26:07 +0800

    Update FormatController.php

--- app/Controller/FormatController.php
+++ app/Controller/FormatController.php
@@ -18,4 +18,12 @@
 // 			$this->set('title',__('Question: Please change Pop Up to mouse over (soft click)'));
 		}
 		
+		public function q1_save() {
+			if (!empty($this->request->data['Type']['type'])) {
+				$this->set('select', $this->request->data['Type']['type']);
+			} else {
+				$this->set('select', __('Nothing'));
+			}
+		}
+		
 	}
