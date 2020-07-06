69a84ec89d7d6318cba04c2f830f8a0a30c18647
hooijing <67728587+hooijing@users.noreply.github.com>
2020-07-06 19:28:20 +0800

    Update OrderReportController.php

--- app/Controller/OrderReportController.php
+++ app/Controller/OrderReportController.php
@@ -1,5 +1,5 @@
 <?php
-	class OrderReportController extends AppController{
+		class OrderReportController extends AppController{
 
 		public function index(){
 
@@ -7,31 +7,51 @@
 
 			$this->loadModel('Order');
 			$orders = $this->Order->find('all',array('conditions'=>array('Order.valid'=>1),'recursive'=>2));
-			// debug($orders);exit;
-
+			
 			$this->loadModel('Portion');
 			$portions = $this->Portion->find('all',array('conditions'=>array('Portion.valid'=>1),'recursive'=>2));
-			// debug($portions);exit;
 
+			
+        foreach ($orders as $order) {
+            $odr = $order['Order'];
+            $order_details = $order['OrderDetail'];
+
+            if ($odr['valid'] == 1) {
+                $result = array();
+                foreach($order_details as $order_detail) {
+                    if ($order_detail['valid'] == 1) {
+                        $quantity = $order_detail['quantity'];
+                        $portion = $portions[$order_detail['item_id']-1];
+                        $portion_details = $portion['PortionDetail'];
+                     foreach ($portion_details as $portion_detail) {
+                              $part = $portion_detail['Part'];
+                         if (empty($result[$part['name']])) {
+                                $result[$part['name']] = 0;
+                            }$result[$part['name']] += $quantity * $portion_detail['value'];
+                        }
+                    }   
+                }$order_reports[$order['Order']['name']] = $result;
+            }
+        }
 
 			// To Do - write your own array in this format
-			$order_reports = array('Order 1' => array(
-										'Ingredient A' => 1,
-										'Ingredient B' => 12,
-										'Ingredient C' => 3,
-										'Ingredient G' => 5,
-										'Ingredient H' => 24,
-										'Ingredient J' => 22,
-										'Ingredient F' => 9,
-									),
-								  'Order 2' => array(
-								  		'Ingredient A' => 13,
-								  		'Ingredient B' => 2,
-								  		'Ingredient G' => 14,
-								  		'Ingredient I' => 2,
-								  		'Ingredient D' => 6,
-								  	),
-								);
+			// $order_reports = array('Order 1' => array(
+										// 'Ingredient A' => 1,
+										// 'Ingredient B' => 12,
+										// 'Ingredient C' => 3,
+										// 'Ingredient G' => 5,
+										// 'Ingredient H' => 24,
+										// 'Ingredient J' => 22,
+										// 'Ingredient F' => 9,
+									// ),
+								  // 'Order 2' => array(
+								  		// 'Ingredient A' => 13,
+								  		// 'Ingredient B' => 2,
+								  		// 'Ingredient G' => 14,
+								  		// 'Ingredient I' => 2,
+								  		// 'Ingredient D' => 6,
+								  	// ),
+								// );
 
 			// ...
 
